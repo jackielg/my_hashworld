70e3e666ebdfc471d2f95407f537b18a1d089deb
jackielg <jackie.lg@hotmail.com>
2018-04-20 22:15:59 +0800

    update

--- Send_email.py
+++ Send_email.py
@@ -33,6 +33,8 @@ def send_mail(to_list, sub, content):
 
 def send_HtmlEmail(to_list, content_list):
 
+    datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
     head = '<!DOCTYPE HTML>' + \
            '<html id="pageLoading">' + \
            '<head>' + \
@@ -66,7 +68,7 @@ def send_HtmlEmail(to_list, content_list):
            '</style>' + \
            '</head>' + \
            '<body>' + \
-           '<p>Hello!</p>' + \
+           '<p> ******** '+ datetime +' ******** </p>' + \
            '<table border="1px" cellspacing="0px" style="border-collapse:collapse" id="table-7">' + \
            '<thead>' + \
            '<th align="center">No.</th>' + \
@@ -96,8 +98,8 @@ def send_HtmlEmail(to_list, content_list):
           str(round(value_total, 2)) + '</td></tr>'
     mail_msg = head + sum + end
 
-    datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    subject = "HashWorld [" + datetime + "], Total [Value:" + str(round(value_total, 2)) + "]"
+
+    subject = "HashWorld, [Value:" + str(round(value_total, 2)) + "]"
 
     msg = MIMEText(mail_msg, 'html', 'utf-8')
     me = "newseeing@163.com"
